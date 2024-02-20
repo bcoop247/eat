1d7e114caae63adb27155962cc24d1c5031576f7
bcoop247 <brcooper247@gmail.com>
2024-02-19 21:49:03 -0500

    bootstrap cdn

--- src/RegistrationForm.jsx
+++ src/RegistrationForm.jsx
@@ -1,4 +1,4 @@
-import "./index.css";
+// import "./index.css";
 import {useState} from 'react'
 
 import React from "react";
@@ -8,35 +8,51 @@ const RegistrationForm = () =>{
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
-  const [password, setPasword] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleFirstName = (e) => {
     setFirstName(e.target.value);
-
+  }
+  const handleLastName = (e) => {
+    setLastName(e.target.value);
+  }
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  }
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
   }
   const handleSubmit = (e) => {
     e.preventDefault();
-    setFirstName(e.target.firstName);
   }
   console.log(firstName);
 
   return(
     <>
     <div className="formContainer">
-      <h1>EAT</h1>
-      <h1>REGISTRATION FORM</h1>
+      <h1 className="header">EAT</h1>
+      <h1 className="header">REGISTRATION FORM</h1>
       <form onSubmit={handleSubmit}>
         <label htmlFor="firstName"></label>
-        <input type="text" onChange={handleFirstName} value={firstName}/>
+        <input id="firstName" type="text" onChange={handleFirstName} value={firstName} placeholder="First Name"/><br />
+
+        <label htmlFor="lastName"></label>
+        <input id="lastName" type="text" onChange={handleLastName} value={lastName} placeholder="Last Name"/><br />
+
+        <label htmlFor="firstName"></label>
+        <input id="password" type="text" onChange={handleEmail} value={email} placeholder="Email"/><br />
+
+        <label htmlFor="firstName"></label>
+        <input id="password" type="text" onChange={handlePassword} value={password} placeholder="Password"/><br />
         
-        <button type="submit"></button>
+        <button type="submit" className="btn btn-primary">Submit</button><br />
       </form>
       
 
 
-<h2>TESTING FIRST NAME = {firstName}</h2>
+{/* <h2>TESTING FIRST NAME = {firstName}</h2>
 <h2>TESTING LAST NAME = {lastName}</h2>
-<h2>TESTING F NAME SUBMITTED: </h2>
+<h2>TESTING F NAME SUBMITTED: </h2> */}
     </div>
     
     </>
