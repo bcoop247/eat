99a06d9317e5c665d10921bd0403aa192f632aeb
bcoop247 <brcooper247@gmail.com>
2024-02-24 13:06:42 -0500

    settings tab

--- src/components/SettingsTab.jsx
+++ src/components/SettingsTab.jsx
@@ -1,12 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGear } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const SettingsTab = () => {
 
   return(
     <>
-    
-    <FontAwesomeIcon icon={faGear} />
+    <div className='container-fluid settingTab'>
+    <FontAwesomeIcon icon={faBars} className='fa-sharp fa-solid fa-2xl mt-2 ms-5'/>
+    </div>
     
     </>
   )
